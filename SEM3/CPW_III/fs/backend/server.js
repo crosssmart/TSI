91193248bb3693aab45236cf3ebe6d952858fde9
Rogerio Filho <rogerio.rhty@outlook.com>
2026-08-27 19:26:33 -0400

    ts omg

---
+++ SEM3/CPW_III/fs/backend/server.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+fs.writeFileSync("texto.txt", "Olá, mundo!");
+
+console.log("Arquivo criado!");
+
--- SEM3/LP/typescript/index.ts
+++ SEM3/LP/typescript/index.ts
@@ -3,4 +3,17 @@ let idade: number = 25;
 let nome: string;
 nome = 'Aderbal';
 
-console.log(nome);
+let ativo: boolean = true;
+
+let notas: number[] = [1,2,3,4];
+
+let pessoa: [string, number] =["joao", 20];
+
+let valor: any = "Pode ser qualquer coisa";
+
+// ou
+let doistipos: number | string = 2
+
+function soma(num1: number, num2: number) {
+  return num1 + num2;
+}
